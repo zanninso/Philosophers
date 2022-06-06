b0ae424976a0cf88cdc0cacfdffbe755ac3b32a1
zanninso <amin_19961@hotmail.com>
2022-06-06 03:52:48 +0100

    improve forks locking system

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -14,15 +14,12 @@
 
 _Bool	init_forks(t_env *env)
 {
-	size_t	i;
-
 	env->forks = malloc(env->philos_number * sizeof(t_fork));
 	if (env->forks)
 	{
 		while (env->nb_init_flocks < env->philos_number)
 		{
-			i = (env->nb_init_flocks + env->nb_init_flocks % 2);
-			env->forks[env->nb_init_flocks].last_user = i % env->philos_number;
+			env->forks[env->nb_init_flocks].status = 1;
 			if (pthread_mutex_init(&env->forks[env->nb_init_flocks].lock, NULL))
 				return (false);
 			env->nb_init_flocks++;
@@ -50,19 +47,23 @@ void	destroy_forks(t_env *env)
 
 _Bool	get_forks(t_philo *philo)
 {
-	_Bool	can_take_forks;
-
 	while (true)
 	{
-		can_take_forks = philo->right_fork->last_user != philo->id;
-		can_take_forks &= philo->left_fork->last_user != philo->id;
 		if ((philo->status == DEAD || *philo->simulation_terminated))
 			return (false);
-		if (can_take_forks)
 		{
 			pthread_mutex_lock(&philo->right_fork->lock);
-			printf(FORK_TAKEN_MSG, get_timestamp(), philo->id);
 			pthread_mutex_lock(&philo->left_fork->lock);
+			if (philo->left_fork->status == 0)
+			{
+				pthread_mutex_unlock(&philo->right_fork->lock);
+				pthread_mutex_unlock(&philo->left_fork->lock);
+				continue;
+			}
+			philo->right_fork->status = 0;
+			philo->left_fork->status = 0;
+			pthread_mutex_unlock(&philo->left_fork->lock);
+			printf(FORK_TAKEN_MSG, get_timestamp(), philo->id);
 			printf(FORK_TAKEN_MSG, get_timestamp(), philo->id);
 			break ;
 		}
@@ -72,9 +73,8 @@ _Bool	get_forks(t_philo *philo)
 
 void	drop_forks(t_philo *philo)
 {
-	philo->right_fork->last_user = philo->id;
-	philo->left_fork->last_user = philo->id;
-	pthread_mutex_unlock(&philo->left_fork->lock);
+	philo->right_fork->status = 1;
+	philo->left_fork->status = 1;
 	pthread_mutex_unlock(&philo->right_fork->lock);
 	printf(FORK_FREED_MSG, get_timestamp(), philo->id);
 	printf(FORK_FREED_MSG, get_timestamp(), philo->id);
